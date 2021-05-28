75443da2c02381e3dda9f534ad2a27740f44addb
Mark Limb <mark@mlpd.com.au>
2021-05-28 15:32:11 +1000

    updated for new song Cutting Onions

--- Scripts/iterate.R
+++ Scripts/iterate.R
@@ -1,7 +1,7 @@
 #### Info ####
 ## This script is to attempt to iterate the production of html files,
 ## for the MailMaker mark down document.
-## The intent is to create a series of unqiue emails that can be sent
+## The intent is to create a series of unique emails that can be sent
 
 library(tidyverse)
 
@@ -18,9 +18,9 @@ library(tidyverse)
 # creates a vector of the radio stations in the table, then sets these names as the path for the output
 station_name <- stations$Station
 mails <- tibble(
-  output_file = stringr::str_c("C:/Users/Prime/Desktop/R Projects/radio_mailer/output/", station_name, "-mailout.html"),
+  output_file = stringr::str_c("C:/Users/limbm/OneDrive - Queensland University of Technology/Documents/R Programs/radio_mailer/output/", station_name, "-mailout.html"),
   params = map(station_name, ~list(station_name = .))
 )
 mails
 mails %>%
-   pwalk(rmarkdown::render, input = "C:/Users/Prime/Desktop/R Projects/radio_mailer/scripts/MailMaker.Rmd")
+   pwalk(rmarkdown::render, input = "C:/Users/limbm/OneDrive - Queensland University of Technology/Documents/R Programs/radio_mailer/Scripts/MailMaker.Rmd")
